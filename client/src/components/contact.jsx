4c6bcf7803ff05bc0d4f1ae9ad141716fec57fd6
vishnu-new-web-student <164169296+vishnu-new-web-student@users.noreply.github.com>
2024-04-05 13:50:57 +0530

    Create contact.jsx

---
+++ client/src/components/contact.jsx
@@ -0,0 +1,72 @@
+
+import { useState, useEffect, useRef } from 'react'
+
+import { useForm } from "react-hook-form"
+import { useNavigate } from 'react-router-dom';
+
+
+
+
+function Conn() {
+  const [count, setCount] = useState()
+  const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors, isSubmitting },
+  } = useForm();
+
+  const delay = (d) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve()
+      }, d * 1000);
+    })
+  }
+
+  //const onSubmit = async (data) => {
+  // await delay(2)
+  //let r = await fetch("http://localhost:3000/", { method: "post", body: JSON.stringify(data) })
+  //let res = await r.text()
+  //console.log(data, res)
+
+
+  const onSubmit = async (data) => {
+    navigate("/Logg")
+    let r = await fetch(import.meta.env.VITE_MONGO, {
+      method: "POST", headers: {
+        "Content-Type": "application/json",
+      }, body: JSON.stringify(data)
+
+      
+    })
+    let res = await r.text()
+   
+    console.log(data, res)
+
+
+  }
+
+  
+
+  return (
+    <>
+      {isSubmitting && <div>Loading...</div>}
+      <div className="container">
+        <form action="" onSubmit={handleSubmit(onSubmit)}>
+          <input placeholder='username'{...register("username")} type="text" name="username" id="" />
+          <br />
+          <input placeholder='password'{...register("password")} type="text" name="password" id="" />
+          <br />
+
+          <button type="submit" value="submit" > submit</button>
+        </form>
+      </div>
+
+    </>
+  )
+}
+
+export default Conn
